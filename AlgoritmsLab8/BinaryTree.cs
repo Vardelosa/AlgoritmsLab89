6f6b5260570612c9ead1e1e87a712085aa326369
vova chacov <vvvoha13@gmail.com>
2019-11-07 17:31:04 +0200

    Добавьте файлы проекта.

---
+++ AlgoritmsLab8/BinaryTree.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmsLab8
+{
+    class BinaryTree
+    {
+        Node head;
+
+        public int count;
+
+        public void Add(int value)
+        {
+            if (head == null)
+                head = new Node(value);
+
+            else
+                StepAdd(head, value);
+
+            count++;
+        }
+        public void StepAdd(Node node, int value)
+        {
+            if(value<node.Data)
+            {
+                if(node.left == null)
+                {
+                    node.left = new Node(value);
+                }
+                else
+                {
+                    StepAdd(node.left, value);
+                }
+            }
+            if(value>node.Data)
+            {
+                if(node.right==null)
+                {
+                    node.right = new Node(value);
+                }
+                else
+                {
+                    StepAdd(node.right, value);
+                }
+            }
+        }
+        public bool Contains(int value)
+        {
+            Node parent;
+
+            return Find(value, out parent) != null;
+        }
+        private Node Find(int value, out Node parent)
+        {
+            Node current = head;
+
+            parent = null;
+
+            while(current!=null)
+            {
+                if(current.Data>value)//Если искомое меньше
+                {
+                    parent = current;
+                    current = current.left;
+                }
+                else if(current.Data<value)//Если искомое больше
+                {
+                    parent = current;
+                    current = current.right;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return current;
+
+        }
+        public void Delete(int value)
+        {
+            Node current = head;
+        }
+
+    }
+}
---
+++ AlgoritmsLab8/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmsLab8
+{
+    class Node
+    {
+        public Node(int value)
+        {
+            Data = value;
+        }
+
+        public Node left { get; set; }
+        public Node right { get; set; }
+        public int Data { get; private set; }
+
+    }
+}
